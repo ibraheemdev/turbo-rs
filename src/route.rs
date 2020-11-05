3dd0b7f2bda34244819da853a94bc3d5a301faa0
Ibraheem <ibrah@1440@gmail.com>
2020-11-05 14:47:53 -0500

    basic route blueprint

--- src/route.rs
+++ src/route.rs
@@ -1,6 +1,8 @@
-use crate::handler::{Extractor, Factory, Handler, MakeService};
-use crate::{Body, FromRequest, Method, Request, Response, Service, StatusCode, ToResponse};
-use futures::future::{ready, BoxFuture, Future};
+use crate::handler::{Extractor, Handler, MakeService};
+use crate::router::RouteConfig;
+use crate::{Body, Request, Response, Service, StatusCode};
+use futures::future::{ready, BoxFuture};
+use std::collections::HashMap;
 
 type BoxedService<Req, Res> = Box<
   dyn Service<
@@ -12,27 +14,28 @@ type BoxedService<Req, Res> = Box<
     + Sync,
 >;
 
-/// Resource Route definition.
-///
-/// Route uses builder-like pattern for configuration.
-pub struct Route {
-  pub method: Option<Method>,
-  pub handler: BoxedService<Request, Response<Body>>,
+/// Stores information to match a request and build URLs.
+pub struct Route<'a> {
+  /// config possibly passed in from `Router`
+  config: RouteConfig,
+
+  /// "global" reference to all named routes
+  named_routes: HashMap<String, &'a Route<'a>>,
+
+  /// Request handler for the route.
+  handler: BoxedService<Request, Response<hyper::Body>>,
+
+  /// If true, this route never matches: it is only used to build URLs.
+  build_only: bool,
+
+  /// The name used to build URLs.
+  name: Option<String>,
 }
 
-impl Route {
-  #[allow(clippy::new_without_default)]
-  /// Create new Route which matches any request
-  /// ```rust
-  /// use turbofish::{Route, Response, Body};
-  ///
-  /// Route::new().to(|| async {
-  ///   Response::new(Body::default())
-  /// });
-  /// ```
-  pub fn new() -> Self {
+impl<'a> Default for Route<'a> {
+  /// Returns an empty route with a not-found handler
+  fn default() -> Self {
     Route {
-      method: None,
       handler: Box::new(MakeService::new(Extractor::new(Handler::new(|| {
         ready(
           Response::builder()
@@ -41,115 +44,126 @@ impl Route {
             .unwrap(),
         )
       })))),
+      config: RouteConfig::default(),
+      named_routes: HashMap::new(),
+      build_only: false,
+      name: None,
     }
   }
+}
 
-  /// Create *Route* for http `GET` requests.
-  /// ```rust
-  /// use turbofish::{Route, Response, Body};
-  ///
-  /// Route::get().to(|| async {
-  ///   Response::new(Body::default())
-  /// });
-  /// ```
-  pub fn method(method: Method) -> Route {
-    Route::new().set_method(method)
-  }
-
-  /// Create *Route* for http `GET` requests.
-  /// ```rust
-  /// use turbofish::{Route, Response, Body};
-  ///
-  /// Route::patch().to(|| async {
-  ///   Response::new(Body::default())
-  /// });
-  /// ```
-  pub fn get() -> Route {
-    Route::new().set_method(Method::GET)
-  }
-
-  /// Create *Route* for http `POST` requests.
-  /// ```rust
-  /// use turbofish::{Route, Response, Body};
-  ///
-  /// Route::post().to(|| async {
-  ///   Response::new(Body::default())
-  /// });
-  /// ```
-  pub fn post() -> Route {
-    Route::new().set_method(Method::POST)
-  }
-
-  /// Create *Route* for http `PUT` requests.
-  /// ```rust
-  /// use turbofish::{Route, Response, Body};
-  ///
-  /// Route::put().to(|| async {
-  ///   Response::new(Body::default())
-  /// });
-  /// ```
-  pub fn put() -> Route {
-    Route::new().set_method(Method::PUT)
-  }
-
-  /// Create *Route* for http `PATCH` requests.
-  /// ```rust
-  /// use turbofish::{Route, Response, Body};
-  ///
-  /// Route::patch().to(|| async {
-  ///   Response::new(Body::default())
-  /// });
-  /// ```
-  pub fn patch() -> Route {
-    Route::new().set_method(Method::PATCH)
-  }
-
-  /// Create *Route* for http `DELETE` requests.
-  /// ```rust
-  /// use turbofish::{Route, Response, Body};
-  ///
-  /// Route::delete().to(|| async {
-  ///   Response::new(Body::default())
-  /// });
-  /// ```
-  pub fn delete() -> Route {
-    Route::new().set_method(Method::DELETE)
-  }
-
-  /// Create *Route* for http `HEAD` requests.
-  /// ```rust
-  /// use turbofish::{Route, Response, Body};
-  ///
-  /// Route::head().to(|| async {
-  ///   Response::new(Body::default())
-  /// });
-  /// ```
-  pub fn head() -> Route {
-    Route::new().set_method(Method::HEAD)
-  }
-
-  /// Set handler function, use request extractors for parameters.
-  /// ```rust
-  /// use turbofish::{Route, Response, Body};
-  ///
-  /// Route::new().to(|| async {
-  ///   Response::new(Body::default())
-  /// });
-  /// ```
-  pub fn to<F, T, R, U>(mut self, handler: F) -> Self
-  where
-    F: Factory<T, R, U> + Send + Sync,
-    T: FromRequest + 'static,
-    R: Future<Output = U> + Send + Sync + 'static,
-    U: ToResponse + 'static,
-  {
-    self.handler = Box::new(MakeService::new(Extractor::new(Handler::new(handler))));
-    self
-  }
-
-  /// Assign the Route to an HTTP Method.
-  pub fn set_method(mut self, method: Method) -> Self {
-    self.method = Some(method);
+impl<'a> Route<'a> {
+  pub fn config(mut self, config: RouteConfig) -> Self {
+    self.config = config;
     self
   }
 }
+//   /// Create *Route* for http `GET` requests.
+//   /// ```rust
+//   /// use turbofish::{Route, Response, Body};
+//   ///
+//   /// Route::get().to(|| async {
+//   ///   Response::new(Body::default())
+//   /// });
+//   /// ```
+//   pub fn method(method: Method) -> Route {
+//     Route::new().set_method(method)
+//   }
+
+//   /// Create *Route* for http `GET` requests.
+//   /// ```rust
+//   /// use turbofish::{Route, Response, Body};
+//   ///
+//   /// Route::patch().to(|| async {
+//   ///   Response::new(Body::default())
+//   /// });
+//   /// ```
+//   pub fn get() -> Route {
+//     Route::new().set_method(Method::GET)
+//   }
+
+//   /// Create *Route* for http `POST` requests.
+//   /// ```rust
+//   /// use turbofish::{Route, Response, Body};
+//   ///
+//   /// Route::post().to(|| async {
+//   ///   Response::new(Body::default())
+//   /// });
+//   /// ```
+//   pub fn post() -> Route {
+//     Route::new().set_method(Method::POST)
+//   }
+
+//   /// Create *Route* for http `PUT` requests.
+//   /// ```rust
+//   /// use turbofish::{Route, Response, Body};
+//   ///
+//   /// Route::put().to(|| async {
+//   ///   Response::new(Body::default())
+//   /// });
+//   /// ```
+//   pub fn put() -> Route {
+//     Route::new().set_method(Method::PUT)
+//   }
+
+//   /// Create *Route* for http `PATCH` requests.
+//   /// ```rust
+//   /// use turbofish::{Route, Response, Body};
+//   ///
+//   /// Route::patch().to(|| async {
+//   ///   Response::new(Body::default())
+//   /// });
+//   /// ```
+//   pub fn patch() -> Route {
+//     Route::new().set_method(Method::PATCH)
+//   }
+
+//   /// Create *Route* for http `DELETE` requests.
+//   /// ```rust
+//   /// use turbofish::{Route, Response, Body};
+//   ///
+//   /// Route::delete().to(|| async {
+//   ///   Response::new(Body::default())
+//   /// });
+//   /// ```
+//   pub fn delete() -> Route {
+//     Route::new().set_method(Method::DELETE)
+//   }
+
+//   /// Create *Route* for http `HEAD` requests.
+//   /// ```rust
+//   /// use turbofish::{Route, Response, Body};
+//   ///
+//   /// Route::head().to(|| async {
+//   ///   Response::new(Body::default())
+//   /// });
+//   /// ```
+//   pub fn head() -> Route {
+//     Route::new().set_method(Method::HEAD)
+//   }
+
+//   /// Set handler function, use request extractors for parameters.
+//   /// ```rust
+//   /// use turbofish::{Route, Response, Body};
+//   ///
+//   /// Route::new().to(|| async {
+//   ///   Response::new(Body::default())
+//   /// });
+//   /// ```
+//   pub fn to<F, T, R, U>(mut self, handler: F) -> Self
+//   where
+//     F: Factory<T, R, U> + Send + Sync,
+//     T: FromRequest + 'static,
+//     R: Future<Output = U> + Send + Sync + 'static,
+//     U: ToResponse + 'static,
+//   {
+//     self.handler = Box::new(MakeService::new(Extractor::new(Handler::new(handler))));
+//     self
+//   }
+
+//   /// Assign the Route to an HTTP Method.
+//   pub fn set_method(mut self, method: Method) -> Self {
+//     self.method = Some(method);
+//     self
+//   }
+// }
